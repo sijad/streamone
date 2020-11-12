9092852beae33bd93a28540bd4e90c81d1abe251
Sajjad Hashemian <hi@hashemian.me>
2020-11-12 15:54:50 +0330

    Revert "add react native paper"
    This reverts commit bc99d70d90eb3d0c08d88e797fb7ee82f174e3c9.

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,3 @@
 module.exports = {
-  plugins: ['optional-require'],
   presets: ['module:metro-react-native-babel-preset'],
-  env: {
-    production: {
-      plugins: ['react-native-paper/babel'],
-    },
-  },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import {StatusBar} from 'react-native';
 import Navigator from 'react-native-easy-router';
-import {Provider as PaperProvider} from 'react-native-paper';
 import * as screens from './screens';
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="rgba(0,0,0,0)" />
-      <PaperProvider>
-        <Navigator screens={screens} initialStack="Home" />
-      </PaperProvider>
+      <Navigator screens={screens} initialStack="Home" />
     </>
   );
 };
