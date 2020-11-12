3b884636ba69400e91e4d4b16e61838a1b3f101f
Sajjad Hashemian <hi@hashemian.me>
2020-11-12 22:14:41 +0330

    implement video player

--- src/screens/Video/index.tsx
+++ src/screens/Video/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import {SafeAreaView, Text, StyleSheet} from 'react-native';
-// import {useVideos} from '../../api';
+import React, {useState, useRef} from 'react';
+import {View, ActivityIndicator, StyleSheet} from 'react-native';
+import RNVideo from 'react-native-video';
 import type {Video as VideoType} from '../../api/rpan/types';
 import {EasyRouterNavigator} from 'react-native-easy-router';
 
@@ -9,13 +9,35 @@ interface VideoProps {
   navigator: EasyRouterNavigator;
 }
 
-export function Video({video}: VideoProps) {
-  // const {data, isLoading, refetch} = useVideos();
+export function Video({
+  video: {
+    broadcast_time,
+    stream: {hls_url, thumbnail},
+  },
+}: VideoProps) {
+  const [isLoading, setLoading] = useState(true);
+  const playerRef = useRef<RNVideo | null>(null);
 
   return (
-    <SafeAreaView style={styles.screen}>
-      <Text>{video.post.id}</Text>
-    </SafeAreaView>
+    <View style={styles.screen}>
+      <RNVideo
+        poster={thumbnail}
+        seek={broadcast_time}
+        onLoad={() => {
+          setLoading(false);
+        }}
+        ref={playerRef}
+        posterResizeMode="cover"
+        resizeMode="cover"
+        style={styles.video}
+        source={{uri: hls_url}}
+      />
+      {isLoading ? (
+        <View style={styles.loading}>
+          <ActivityIndicator color="#fff" size="large" />
+        </View>
+      ) : null}
+    </View>
   );
 }
 
@@ -23,7 +45,13 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
   },
-  listContainer: {
-    justifyContent: 'space-evenly',
+  video: {
+    flex: 1,
+  },
+  loading: {
+    backgroundColor: 'rgba(0,0,0,0.6)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    ...StyleSheet.absoluteFillObject,
   },
 });
