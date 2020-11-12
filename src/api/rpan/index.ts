ac20215cb918cff188454852be7cddf311c8de29
Sajjad Hashemian <hi@hashemian.me>
2020-11-12 21:49:57 +0330

    use proper key

--- src/api/rpan/index.ts
+++ src/api/rpan/index.ts
@@ -4,7 +4,7 @@ import type {VideoResponse, VideosResponse} from './types';
 const BASE_URL = 'https://strapi.reddit.com';
 
 export function useVideo(id: string) {
-  return useQuery<VideoResponse>(`video-${id}`, async () =>
+  return useQuery<VideoResponse>(['video', id], async () =>
     fetch(`${BASE_URL}/videos/${id}`).then((res) => res.json()),
   );
 }
@@ -12,7 +12,7 @@ export function useVideo(id: string) {
 export function useVideos(subreddit: string | undefined = '') {
   // TODO figure it out how broadcasts paginations works
   // and use useInfiniteQuery instead
-  return useQuery<VideosResponse>(`videos-${subreddit}`, async () =>
+  return useQuery<VideosResponse>(['videos', subreddit], async () =>
     fetch(
       `${BASE_URL}${subreddit ? `/r/${subreddit}` : ''}/broadcasts`,
     ).then((res) => res.json()),
