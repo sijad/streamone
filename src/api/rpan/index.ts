bf8046282305457e5ea3aa68a875640a6171bea6
Sajjad Hashemian <hi@hashemian.me>
2020-11-13 20:46:48 +0330

    add proper user agent

--- src/api/rpan/index.ts
+++ src/api/rpan/index.ts
@@ -1,20 +1,28 @@
 import {useQuery} from 'react-query';
 import type {VideoResponse, VideosResponse} from './types';
+import {version} from '../../../package.json';
 
-const BASE_URL = 'https://strapi.reddit.com';
+const STR_BASE_URL = 'https://strapi.reddit.com';
+
+async function getJSON(url: string) {
+  return fetch(url, {
+    headers: {
+      'user-agent': `streamone/${version}`,
+    },
+    keepalive: true,
+  }).then((res) => res.json());
+}
 
 export function useVideo(id: string) {
   return useQuery<VideoResponse>(['video', id], async () =>
-    fetch(`${BASE_URL}/videos/${id}`).then((res) => res.json()),
+    getJSON(`${STR_BASE_URL}/videos/${id}`),
   );
 }
 
-export function useVideos(subreddit: string | undefined = '') {
+export function useVideos(subreddit: string | undefined = undefined) {
   // TODO figure it out how broadcasts paginations works
   // and use useInfiniteQuery instead
   return useQuery<VideosResponse>(['videos', subreddit], async () =>
-    fetch(
-      `${BASE_URL}${subreddit ? `/r/${subreddit}` : ''}/broadcasts`,
-    ).then((res) => res.json()),
+    getJSON(`${STR_BASE_URL}${subreddit ? `/r/${subreddit}` : ''}/broadcasts`),
   );
 }
